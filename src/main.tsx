58b6e1fc12a9d303513dd2e6f064c814ee3f835a
nshuti <nshutikope@gmail.com>
2026-03-18 19:22:10 +0200

    Add forms and user components, update assets and gitignore

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Header from "../components/Header";
-import List from "../components/List";
-import Mains from "../components/Mains";
+// import Header from "../components/Header";
+// import List from "../components/List";
+// import Mains from "../components/Mains";
+import User from "../components/User";
+import Forms from "../components/Forms";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Header />
-    <Mains />
-    <List />
+
+    < Forms />
   </StrictMode>,
 );
