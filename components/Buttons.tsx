47878801d82b738bf5896363dca09c0d143cc2bf
nshuti <nshutikope@gmail.com>
2026-03-24 17:55:33 +0200

    Add state management to Buttons component; implement turnOffAll function to reset pad states

--- components/Buttons.tsx
+++ components/Buttons.tsx
@@ -2,20 +2,24 @@
 
 import React from "react";
 import { Pads } from "../src/assets/Datas/Pads";
-import Padz from "./Padz"
+import Padz from "./Padz";
 
 export default function Buttons() {
-  const [pads] = React.useState(Pads);
+  const [pads, setPads] = React.useState(Pads);
 
   const buttonElements = pads.map((pad) => {
     return <Padz key={pad.id} color={pad.color} id={pad.id} on={pad.on} />;
   });
+    function turnOffAll() {
+        setPads((previousPads) => previousPads.map(item => ({ ...item, on: false }))
+        )
+    }
 
   return (
     <main>
-      <div className='pad-container'>
-        {buttonElements}
-      </div>
+      <div className='pad-container'>{buttonElements}</div>
+
+      <button onClick={turnOffAll}>Turnoff All</button>
     </main>
   );
 }
