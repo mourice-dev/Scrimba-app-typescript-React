e6d8c8869f751cc6292048baee2a9888b3d78e4a
nshuti <nshutikope@gmail.com>
2026-03-31 20:05:44 +0200

    Rename WindowSize component to WindowTracker and refactor state management for window width

--- components/WindowSize.tsx
+++ components/WindowSize.tsx
@@ -1,21 +1,23 @@
 /** @format */
 
-import { useEffect, useState } from "react";
+import React from "react";
 
-export default function WindowSize() {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+export default function WindowTracker() {
+  /**
+   * Challenge:
+   * 1. Create state called `windowWidth`, default to
+   *    `window.innerWidth`
+   * 2. When the window width changes, update the state
+   * 3. Display the window width in the h1 so it updates
+   *    every time it changes
+   */
+  const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
 
-  useEffect(() => {
-    function watchWidth() {
+  React.useEffect(() => {
+    window.addEventListener("resize", function () {
       setWindowWidth(window.innerWidth);
-    }
-
-    window.addEventListener("resize", watchWidth);
-
-    return function cleanup() {
-      window.removeEventListener("resize", watchWidth);
-    };
-  }, []);
+    });
+  }, [window.innerWidth]);
 
   return <h1>Window width: {windowWidth}</h1>;
 }
