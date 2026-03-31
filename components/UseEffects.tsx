86f3187c763c619abff835218674aa75b96f7351
nshuti <nshutikope@gmail.com>
2026-03-31 17:44:10 +0200

    Add SideEffects component to manage API data fetching and state

---
+++ components/UseEffects.tsx
@@ -0,0 +1,30 @@
+/** @format */
+
+import React from "react";
+
+export default function SideEffects() {
+  const [starWarsData, setStarWarsData] = React.useState({});
+  const [count, setCount] = React.useState(0);
+
+  /**
+   * Challenge part 1:
+   * Fetch the data from this url: "https://swapi.dev/api/people/1"
+   * and save it in the starWarsData state. Make sure you don't
+   * get stuck in an infinite rendering loop!
+   */
+  React.useEffect(() => {
+    fetch(`https://swapi.dev/api/people/${count}`)
+      .then((res) => res.json())
+      .then((data) => setStarWarsData(data));
+  }, [count]);
+
+  return (
+    <div>
+      <h2>The count is {count}</h2>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Add
+      </button>
+      <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+    </div>
+  );
+}
